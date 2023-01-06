c1a8fa96f5fcad26af733638dc84f1104f655d44
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2023-01-06 18:05:50 +0700

    add options controller

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onReturnToMainMenu()
+    {
+        audio.Play();
+        Debug.Log("Return");
+    }
+}
